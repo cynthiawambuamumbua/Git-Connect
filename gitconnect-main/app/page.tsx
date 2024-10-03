aaba14018ce2b6c12f8ff00a6dd1ab5c43b6d46c
cynthiawambuamumbua <cynthiamumbuawambua@gmail.com>
2024-10-03 15:53:36 +0300

    edited page.tsx

--- gitconnect-main/app/page.tsx
+++ gitconnect-main/app/page.tsx
@@ -1,10 +1,10 @@
 import Auth from "./signup/page";
-import EditProfile from "./profile/edit-profile/id";
-import Profile from "./profile/id";
+// import EditProfile from "./profile/edit-profile/id";
+// import Profile from "./profile/id";
 import Developers from "./pages/developers";
 import Navbar from './components/navbar/Navbar'
-import Footer from "./components/footer/Footer";
 import DeveloperPosts from "./components/posts/DeveloperPosts";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
@@ -12,8 +12,8 @@ export default function Home() {
       <Navbar />
       <DeveloperPosts />
        {/* <Auth/> */}
-      <EditProfile/>
-      <Profile/>
+      {/* <EditProfile/> */}
+      {/* <Profile/> */}
       {/* <Developers/>  */}
       
       <Footer />
